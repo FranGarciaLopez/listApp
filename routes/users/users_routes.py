d0715dd9cbe7b14f757ab92103e4d79e9af57ed4
Francisco Garcia <frangalo34@gmail.com>
2023-02-02 19:44:47 +0100

    endpoint

--- routes/users/users_routes.py
+++ routes/users/users_routes.py
@@ -7,3 +7,8 @@ from controllers.users import Users
 @app.post("/users", description="This endpoint creates a new user in the database")
 async def add_user_route(data: Dict[str, Any]  = {'name': 'John', 'surname': 'Doe', 'password': 'test'}):
     return Users(engine).create_user(data)
+
+
+@app.get("/users", description="This endpoint gets all the users in the database")
+async def get_all_users_route():
+    return await Users(engine).get_all_users()
